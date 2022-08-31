313ccfadfde774295edbb5c0dbd3326abe2dcb39
Tanuj Upreti <tanuj@zaycare.com>
2022-08-31 20:18:30 +0530

    Added local machine visualizer

--- src/App.js
+++ src/App.js
@@ -1,89 +1,99 @@
-import './App.css';
-import {useEffect, useState} from "react";
-import {useMachine} from '@xstate/react';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { useMachine } from "@xstate/react";
+import { inspect } from "@xstate/inspect";
 import messagesMachine from "./machines/chat";
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
+inspect({
+  // options
+  url: "https://stately.ai/viz?inspect", // (default)
+  iframe: false, // open in new window
+});
 
 const messages = [];
 
 function App() {
-    const [state, send] = useMachine(messagesMachine, {
-        services: {
-            loadMessages: async () => {
-                return messages;
-            },
-            saveMessage: async (context, event) => {
-                console.log("messages",messages)
-                messages.push(context.createNewTodoFormInput);
-            },
+  const [state, send] = useMachine(messagesMachine, {
+    devTools: true,
+    services: {
+      loadMessages: async () => {
+        return messages;
+      },
+      saveMessage: async (context, event) => {
+        console.log("messages", messages);
+        messages.push(context.createNewTodoFormInput);
+      },
+    },
+  });
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    send("initialise");
+  }, []);
 
-        },
-    });
-    const [message, setMessage] = useState("")
-    const [messages, setMessages] = useState([])
-    useEffect(() => {
-        send('initialise')
-    }, [])
+  console.log("state", state.event.type);
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">Chat App</h1>
+      </div>
 
-    console.log("state",state.event.type)
-    return (
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">Chat App</h1>
-            </div>
-
-            <div className="container-fluid bg-light" id="chat">
-                <div className="content d-flex flex-column" id="chat-content">
-                    {state.context.messages?.map(msg => {
-                        return <span id="msg-0" className="msg">
-                                    <div className="head"> User </div>
-                                    <p className="body"> {msg} </p>
-                                    <div className="footer"> 8:44:37 AM </div>
-                                </span>
-                    })}
-                </div>
-                {state.matches("Messages Loaded") && (
-                    <button
-                        className="btn btn-primary "
-                        onClick={() => {
-                            send({
-                                type: "Create new",
-                            });
-                        }}
-                    >
-                        Create new
-                    </button>
-                )}
-                {state.matches("Creating new message.Showing form input") && (
-                    <>
-
-                        <form className="tools form-row"
-                              onSubmit={(e) => {
-                                  e.preventDefault();
-                                  send({
-                                      type: "Submit",
-                                  });
-                              }}
-                        >
-                            {state.event.type==="Form input changed"  && <p className="typing">Typing...</p>}
-                            <input
-                                placeholder="Type message..."
-                                id="newMessage" className="form-control col mr-2"
-                                onChange={(e) => {
-                                    send({
-                                        type: "Form input changed",
-                                        value: e.target.value,
-                                    });
-                                }}
-                            />
-                        </form>
-                    </>
-                )}
-
-            </div>
+      <div className="container-fluid bg-light" id="chat">
+        <div className="content d-flex flex-column" id="chat-content">
+          {state.context.messages?.map((msg) => {
+            return (
+              <span id="msg-0" className="msg">
+                <div className="head"> User </div>
+                <p className="body"> {msg} </p>
+                <div className="footer"> 8:44:37 AM </div>
+              </span>
+            );
+          })}
         </div>
-    );
+        {state.matches("Messages Loaded") && (
+          <button
+            className="btn btn-primary "
+            onClick={() => {
+              send({
+                type: "Create new",
+              });
+            }}
+          >
+            Create new
+          </button>
+        )}
+        {state.matches("Creating new message.Showing form input") && (
+          <>
+            <form
+              className="tools form-row"
+              onSubmit={(e) => {
+                e.preventDefault();
+                send({
+                  type: "Submit",
+                });
+              }}
+            >
+              {state.event.type === "Form input changed" && (
+                <p className="typing">Typing...</p>
+              )}
+              <input
+                placeholder="Type message..."
+                id="newMessage"
+                className="form-control col mr-2"
+                onChange={(e) => {
+                  send({
+                    type: "Form input changed",
+                    value: e.target.value,
+                  });
+                }}
+              />
+            </form>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/machines/chat.js
+++ src/machines/chat.js
@@ -1,91 +1,91 @@
 import { createMachine, assign, interpret } from "xstate";
 
 const messagesMachine = createMachine(
-    {
-        context: {
-            messages: [],
-            createNewTodoFormInput: "",
-        },
-        id: "Chat machine",
-        initial: "LOADING_MESSAGES",
-        states: {
-            LOADING_MESSAGES: {
-                invoke: {
-                    src: "loadMessages",
-                    onDone: [
-                        {
-                            actions: "assignMessagesToContext",
-                            cond: "Has messages",
-                            target: "Messages Loaded",
-                        },
-                        {
-                            target: "Creating new message",
-                        },
-                    ],
-                },
-            },
-            "Messages Loaded": {
-                on: {
-                    "Create new": {
-                        target: "Creating new message",
-                    },
-                },
-            },
-            "Loading messages errored": {},
-            "Creating new message": {
-                initial: "Showing form input",
-                states: {
-                    "Showing form input": {
-                        on: {
-                            "Form input changed": {
-                                actions: "assignFormInputToContext",
-                            },
-                            Submit: {
-                                target: "Saving message",
-                            },
-                        },
-                    },
-                    "Saving message": {
-                        invoke: {
-                            src: "saveMessage",
-                            onDone: [
-                                {
-                                    target: "#Chat machine.LOADING_MESSAGES",
-                                },
-                            ],
-                        },
-                    },
-                },
-            },
-        },
+  {
+    context: {
+      messages: [],
+      createNewTodoFormInput: "",
     },
-    {
-        guards: {
-            "Has messages": (context, event) => {
-                return event.data.length > 0;
+    id: "Chat machine",
+    initial: "LOADING_MESSAGES",
+    states: {
+      LOADING_MESSAGES: {
+        invoke: {
+          src: "loadMessages",
+          onDone: [
+            {
+              actions: "assignMessagesToContext",
+              cond: "Has messages",
+              target: "Messages Loaded",
             },
+            {
+              target: "Creating new message",
+            },
+          ],
         },
-        actions: {
-            assignMessagesToContext: assign((context, event) => {
-                return {
-                    messages: event.data,
-                };
-            }),
-            assignErrorToContext: assign((context, event) => {
-                return {
-                    errorMessage: event.data.message,
-                };
-            }),
-            assignFormInputToContext: assign((context, event) => {
-                return {
-                    createNewTodoFormInput: event.value,
-                };
-            }),
+      },
+      "Messages Loaded": {
+        on: {
+          "Create new": {
+            target: "Creating new message",
+          },
         },
-    }
+      },
+      //   "Loading messages errored": {},
+      "Creating new message": {
+        initial: "Showing form input",
+        states: {
+          "Showing form input": {
+            on: {
+              "Form input changed": {
+                actions: "assignFormInputToContext",
+              },
+              Submit: {
+                target: "Saving message",
+              },
+            },
+          },
+          "Saving message": {
+            invoke: {
+              src: "saveMessage",
+              onDone: [
+                {
+                  target: "#Chat machine.LOADING_MESSAGES",
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    guards: {
+      "Has messages": (context, event) => {
+        return event.data.length > 0;
+      },
+    },
+    actions: {
+      assignMessagesToContext: assign((context, event) => {
+        return {
+          messages: event.data,
+        };
+      }),
+      assignErrorToContext: assign((context, event) => {
+        return {
+          errorMessage: event.data.message,
+        };
+      }),
+      assignFormInputToContext: assign((context, event) => {
+        return {
+          createNewTodoFormInput: event.value,
+        };
+      }),
+    },
+  }
 );
 const service = interpret(messagesMachine).onTransition((state) => {
-    console.log(state.value);
+  console.log(state.value);
 });
 service.start();
 
